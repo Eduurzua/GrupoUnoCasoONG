3541c27ba5e82eef6db6f92a3f90d735bd470632
Eduardo Urzúa <eduardo.urzua.padilla@gmail.com>
2022-06-13 16:03:13 -0400

    TA01-8
    Redirigir la app desde el Log In al Sign Up cuando se presione su botón en la vista de inicio de sesión

---
+++ app/src/main/java/com/example/ongsomosmas/Fragment/FragmentLogin.kt
@@ -0,0 +1,25 @@
+package com.example.ongsomosmas.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.ongsomosmas.R
+import com.example.ongsomosmas.databinding.ActivityLoginBinding
+
+
+class FragmentLogin : Fragment() {
+
+    private lateinit var binding : ActivityLoginBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = ActivityLoginBinding.inflate(inflater, container,false)
+
+        binding.btn_login.setOnClickListener {
+            findNavController().navigate(R.id.action_login_to_signUp)
+        }
+        return binding.root
+    }
+}
