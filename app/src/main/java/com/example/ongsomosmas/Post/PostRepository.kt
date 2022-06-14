04bb455e9d848d931ca6a96da118d40775d9624e
Eduardo Urzúa <eduardo.urzua.padilla@gmail.com>
2022-06-14 14:53:53 -0400

    ultimos cambios
    ultimos cambios

--- app/src/main/java/com/example/ongsomosmas/Post/PostRepository.kt
+++ app/src/main/java/com/example/ongsomosmas/Post/PostRepository.kt
@@ -1,16 +1,10 @@
 package com.example.ongsomosmas.Post
 
+import com.example.ongsomosmas.Model.Register
+
 class PostRepository(
 
     private val remoteDataSource: PostRemoteDataSource
 ) {
 
-    fun getPost(id: Int, listener: ResponseListener<Post>) {
-        this.remoteDataSource.getPost(id,listener)
-    }
-
-
-    fun createUser(str: String, listener: ResponseListener<Post>) {
-        this.remoteDataSource.getPosts(listener)
-    }
 }
--- app/src/main/java/com/example/ongsomosmas/Post/PostService.kt
+++ app/src/main/java/com/example/ongsomosmas/Post/PostService.kt
@@ -1,17 +1,12 @@
 package com.example.ongsomosmas.Post
 
-import com.example.ongsomosmas.Model.User
-import retrofit2.Call
+import com.example.ongsomosmas.Model.Register
+import okhttp3.ResponseBody
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 class PostService {
-    @GET("/login")
-    fun loginUser(@Query("participants") int : Int): Call<Post>
-
-
     @POST("/register")
-    fun registerUser(@Body user: User): Call<User>
+    fun registerUser(@Body Register: Register) : ResponseBody<Register>
+
 }
