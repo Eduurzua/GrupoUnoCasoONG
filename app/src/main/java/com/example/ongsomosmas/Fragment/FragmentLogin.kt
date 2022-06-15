f70b518907dbded0644c6c79ade4184904dd828d
Diana Cerda <52170526+Daifuku33@users.noreply.github.com>
2022-06-14 21:58:42 -0400

    avance validacion login

--- app/src/main/java/com/example/ongsomosmas/Fragment/FragmentLogin.kt
+++ app/src/main/java/com/example/ongsomosmas/Fragment/FragmentLogin.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.util.PatternsCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -14,6 +16,7 @@ import com.example.ongsomosmas.R
 import com.example.ongsomosmas.databinding.ActivityLoginBinding
 import com.example.ongsomosmas.views.MainViewModel
 import com.example.ongsomosmas.views.VideModelFactory
+import java.util.regex.Pattern
 
 
 class FragmentLogin : Fragment() {
@@ -24,25 +27,25 @@ class FragmentLogin : Fragment() {
     )
 
     private val loginViewModel : LoginViewModel by viewModels()
+    val result : Boolean = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = ActivityLoginBinding.inflate(inflater, container,false)
 
+        /*el boton comienza desactivado*/
+        binding.btnLogin.isEnabled = false
+
+
         binding.btSingUp.setOnClickListener {
             findNavController().navigate(R.id.action_login_to_signUp)
         }
-        
-        /*el boton comienza desactivado*/
-        /*binding.btnLogin.isEnabled = false*/
 
         binding.btnLogin.setOnClickListener {
-            Toast.makeText(context, "boton login apretado", Toast.LENGTH_SHORT).show()
-            println("mensaje")
-        }
-
-        binding.btnLogin.setOnClickListener {
-            viewModel.loginUser(Login(binding.tiEmail.editText?.text.toString(),binding.tiPassword.editText?.text.toString()))
-            //Toast.makeText(context, "error", Toast.LENGTH_SHORT).show()
+            if (validate()) {
+                viewModel.loginUser(Login(binding.tiEmail.editText?.text.toString(),binding.tiPassword.editText?.text.toString()))
+            }else {
+                Toast.makeText(context, "Error de login", Toast.LENGTH_SHORT).show()
+            }
         }
 
         viewModel.login.observe(viewLifecycleOwner) { value ->
@@ -59,6 +62,47 @@ class FragmentLogin : Fragment() {
             }
         }
         return binding.root
+    }
 
+    private fun validate(): Boolean {
+        val result = arrayOf(validateEmail(), validatePassword())
+        return false !in result
+    }
+
+    private fun validateEmail() : Boolean {
+        val email = binding.tiEmail.editText?.text.toString()
+        return if (email.isEmpty()){
+            binding.tiEmail.error = "Campo email no puede estar vacío"
+            false
+        }else if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.tiEmail.error = "Campo email inválido"
+            false
+        }else{
+            binding.tiEmail.error = null
+            true
+        }
+    }
+
+    private fun validatePassword() : Boolean {
+        val password = binding.tiPassword.editText?.text.toString()
+        val passwordRegex = Pattern.compile(
+            "^"+
+                    "(?=.*[0-9])"+          //al menos 1 dígito
+                    "(?=.*[A-Z])"+          //al menos una minúscula
+                    "(?=.*[@#$%^&+=!])"+    //al menos un simbolo especial
+                    "(?=\\S+$)"+            //sin espacios
+                    ".{8,}"+                //minimo 8 de largo
+                    "$"
+        )
+        return if (password.isEmpty()){
+            binding.tiPassword.error = "Campo contraseña no puede estar vacío"
+            false
+        }else if (!passwordRegex.matcher(password).matches()){
+            binding.tiPassword.error = "La contraseña es débil"
+            false
+        }else {
+            binding.tiPassword.error = null
+            true
+        }
     }
 }
--- app/src/main/java/com/example/ongsomosmas/Fragment/LoginViewModel.kt
+++ app/src/main/java/com/example/ongsomosmas/Fragment/LoginViewModel.kt
@@ -4,8 +4,7 @@ import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
     val EMAIL_REGEX = "^[A-Za-z](.*)([@]{1})(\\.)(.{1,})"
-    val PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$" /*largo 8*/
-
+    val PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$"
 
     fun validateEmail(email: String) : Boolean{
         val result = EMAIL_REGEX.toRegex().matches(email)
@@ -15,18 +14,14 @@ class LoginViewModel : ViewModel() {
     }
 
     fun validatePassword(password: String) : Boolean{
-        val result = PASSWORD_REGEX.toRegex().matches(password)
-        return if(password.isEmpty()){
+        val result : Boolean
+        return if (password.length < 8){
             false
-        }else result
+        } else if (password.isEmpty()) {
+            false
+        } else {
+            result = PASSWORD_REGEX.toRegex().matches(password)
+            result
+        }
     }
-
-    //*buscar mejor validador de boleanos*//
-    fun login(email:String, password: String) {
-        val resultEmail = validateEmail(email)
-        val resultPassword = validatePassword(password)
-
-        /*si ambos son true */
-    }
-
 }
