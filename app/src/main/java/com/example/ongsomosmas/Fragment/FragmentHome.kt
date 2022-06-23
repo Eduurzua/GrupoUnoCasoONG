9715adaddeb2ead1e9c8ff8918be4ec3a80108fd
Eduardo Urzúa <eduardo.urzua.padilla@gmail.com>
2022-06-22 20:56:46 -0400

    Squashed commit of the following:
    commit a7de27067edfe90c813622b69883fd4d14cc17c4
    Author: Diana Cerda <52170526+Daifuku33@users.noreply.github.com>
    Date:   Wed Jun 22 14:28:55 2022 -0400
    
        Ultima corrección tamaño letra
    
    commit 2ec7f4d822cfab9de4523601bfc5c09ee646a71c
    Author: Diana Cerda <52170526+Daifuku33@users.noreply.github.com>
    Date:   Wed Jun 22 14:22:02 2022 -0400
    
        Vista Main con menu probado y funcionando
    
    commit 8528b556591b924330240465e8904a35509e21cd
    Author: Diana Cerda <52170526+Daifuku33@users.noreply.github.com>
    Date:   Wed Jun 22 14:17:07 2022 -0400
    
        Revert "Vista Main con menu probado y funcionando"
    
        This reverts commit 141d4b33f643676540e5b1c1cb3bce3ffbf67768.
    
    commit 141d4b33f643676540e5b1c1cb3bce3ffbf67768
    Author: Diana Cerda <52170526+Daifuku33@users.noreply.github.com>
    Date:   Wed Jun 22 14:13:29 2022 -0400
    
        Vista Main con menu probado y funcionando
    
    commit 451c8b6b5be0f13e145d771da5eb8b7fec4b3a71
    Author: Diana Cerda <52170526+Daifuku33@users.noreply.github.com>
    Date:   Tue Jun 21 18:12:12 2022 -0400
    
        avance vista Main Menu lista (no probada)
    
    commit eb308b995b611d343899246859b2175dcaca1dbd
    Author: Diana Cerda <52170526+Daifuku33@users.noreply.github.com>
    Date:   Mon Jun 20 19:58:38 2022 -0400
    
        vista Main Menu lista (no probada)

--- app/src/main/java/com/example/ongsomosmas/Fragment/FragmentHome.kt
+++ app/src/main/java/com/example/ongsomosmas/Fragment/FragmentHome.kt
@@ -1,39 +1,47 @@
 package com.example.ongsomosmas.Fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.activity.addCallback
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import com.example.ongsomosmas.R
-import com.example.ongsomosmas.databinding.FragmentHomeBinding
-import com.example.ongsomosmas.views.MainViewModel
-import com.example.ongsomosmas.views.VideModelFactory
+import com.example.ongsomosmas.databinding.ActivityHomeBinding
+
 
 class FragmentHome : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
-    private val viewModel: MainViewModel by viewModels(
-        factoryProducer = { VideModelFactory(requireContext()) }
-    )
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+    ): View? {
+        this.setHasOptionsMenu(true)
 
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
-            activity?.finish()
+        binding = ActivityHomeBinding.inflate(inflater, container, false)
+
+        /*Visibilidades de menu*/
+        binding.MenuButton.setOnClickListener() {
+            binding.menu.visibility = View.VISIBLE
+            binding.MenuButton.visibility = View.GONE
+        }
+        binding.iconClear.setOnClickListener() {
+            binding.menu.visibility = View.GONE
+            binding.MenuButton.visibility = View.VISIBLE
         }
 
-        binding.button3.setOnClickListener(){
-            findNavController().navigate(R.id.action_home_to_news)
+        /*Opciones menu*/
+        binding.iconContact.setOnClickListener() {
+            Toast.makeText(context, "Redirigiendo a contactos", Toast.LENGTH_SHORT).show()
         }
+        binding.iconNews.setOnClickListener() {
+            Toast.makeText(context, "Redirigiendo a novedades", Toast.LENGTH_SHORT).show()
+        }
+        binding.iconStaff.setOnClickListener() {
+            Toast.makeText(context, "Redirigiendo a Staff", Toast.LENGTH_SHORT).show()
+        }
+
+        /*ojo como vuelve hacia atrás cuando esté implementado*/
 
         return binding.root
     }
